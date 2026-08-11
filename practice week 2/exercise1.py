4b2576e9857cd50b78f3eeb9ca6fd3f52f3be4b2
Omer Ahmad <soulcraftsucks@gmail.com>
2026-08-11 22:22:17 +0500

    12

--- practice week 2/exercise1.py
+++ practice week 2/exercise1.py
@@ -1,8 +1,18 @@
 def main():
 
-    with open(notes.txt, "r") as file:
+    with open("notes.txt", "r") as file:
         text = file.read()
         print(text)
+
+    with open("notes.txt", "a") as file:
+        file.write("Omer studies\n")
+        file.write("omer plays\n")
+
+    with open("notes.txt", "r") as file:
+        reader = file.readlines()
+        for line in reader:
+            print(line.strip())
+
 
 if __name__ == "__main__":
     main()
