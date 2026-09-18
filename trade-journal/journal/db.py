93473b2f0af81a36b75d55ce2e04beb56ae1beec
Omer Ahmad <soulcraftsucks@gmail.com>
2026-09-18 19:56:25 +0500

    Add Postgres connection setup with SQLAlchemy engine

---
+++ trade-journal/journal/db.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+
+load_dotenv()
+
+password = os.getenv("DB_PASSWORD")
+encoded_password = quote_plus(password)
+
+DATABASE_URL = f"postgresql+psycopg2://postgres:{encoded_password}@localhost:5432/trade_journal"
+
+engine = create_engine(DATABASE_URL)
+
+if __name__ == "__main__":
+    with engine.connect() as connection:
+        print("connection established successfully!")
